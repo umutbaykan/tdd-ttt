e6d21a3acc5369aa853a41718d6aec7b9d07d932
umut <umut_baykan@hotmail.com>
2023-07-26 21:12:03 +0100

    InputContainer renders form and button components

--- src/component/inputContainer/InputContainer.cy.jsx
+++ src/component/inputContainer/InputContainer.cy.jsx
@@ -4,4 +4,10 @@ describe("InputContainer component", () => {
   it("mounts", () => {
     cy.mount(<InputContainer />);
   });
+
+  it("renders InputButton and Form components", () => {
+    cy.mount(<InputContainer />);
+    cy.getByCy("input-button").should("exist");
+    cy.getByCy("input-form").should("exist");
+  });
 });
--- src/component/inputContainer/InputContainer.jsx
+++ src/component/inputContainer/InputContainer.jsx
@@ -1,7 +1,15 @@
 import "./InputContainer.css";
 
+import Form from "../form/From";
+import InputButton from "../inputButton/InputButton";
+
 const InputContainer = () => {
-  return <p>Hi</p>;
+  return (
+    <div className="container form">
+      <Form />
+      <InputButton />
+    </div>
+  );
 };
 
 export default InputContainer;
