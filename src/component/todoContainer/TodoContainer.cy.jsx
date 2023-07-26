d71a7f3e41e4bde871e36cb3f14a755c5f9486d7
umut <umut_baykan@hotmail.com>
2023-07-26 22:00:03 +0100

    TodoContainer to render a list of todos

--- src/component/todoContainer/TodoContainer.cy.jsx
+++ src/component/todoContainer/TodoContainer.cy.jsx
@@ -1,7 +1,20 @@
 import TodoContainer from "./TodoContainer";
 
 describe("TodoContainer component", () => {
+  let todos;
+
+  beforeEach(() => {
+    cy.fixture("todos").then((data) => {
+      todos = data.todos;
+    });
+  });
+
   it("mounts", () => {
-    cy.mount(<TodoContainer />);
+    cy.mount(<TodoContainer todos={todos} />);
+  });
+
+  it("renders a list of todos", () => {
+    cy.mount(<TodoContainer todos={todos} />);
+    cy.getByCyLike("todo").should("have.length", 2);
   });
 });
--- src/component/todoContainer/TodoContainer.jsx
+++ src/component/todoContainer/TodoContainer.jsx
@@ -1,7 +1,17 @@
 import "./TodoContainer.css";
+import propTypes from "prop-types";
+import Todo from "../todo/Todo";
 
-const TodoContainer = () => {
-  return <p>Hi</p>;
+const TodoContainer = ({ todos }) => {
+  return (
+    <div className="container todo">
+      {todos.map((entry, index) => (
+        <Todo key={index} text={entry.todo} index={index} isDone={false} />
+      ))}
+    </div>
+  );
 };
+
+TodoContainer.propTypes = { todos: propTypes.array };
 
 export default TodoContainer;
