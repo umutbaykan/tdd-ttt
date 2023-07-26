3a70a8998c98be4f2973dd983706c14dd4d16f30
umut <umut_baykan@hotmail.com>
2023-07-26 20:52:11 +0100

    InputButton component initialization

---
+++ src/component/inputButton/InputButton.cy.jsx
@@ -0,0 +1,7 @@
+import InputButton from "./InputButton";
+
+describe("InputButton component", () => {
+  it("mounts", () => {
+    cy.mount(<InputButton />);
+  });
+});
---
+++ src/component/inputButton/InputButton.jsx
@@ -0,0 +1,7 @@
+import "./InputButton.css";
+
+const InputButton = () => {
+  return <button>Hi</button>;
+};
+
+export default InputButton;
