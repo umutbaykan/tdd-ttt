1b216987da0d3f7ed4a14407e32a446d871aa9db
umut <umut_baykan@hotmail.com>
2023-07-26 21:04:13 +0100

    InputButton API call to back end with prop data

--- src/component/inputButton/InputButton.cy.jsx
+++ src/component/inputButton/InputButton.cy.jsx
@@ -9,4 +9,17 @@ describe("InputButton component", () => {
     cy.mount(<InputButton />);
     cy.getByCy("input-button").should("have.text", "Add Note!");
   });
+
+  it("makes an API call with the props it has been passed in", () => {
+    cy.intercept("POST", "/post", []).as("notePostRequest");
+    cy.get("@notePostRequest.all").should("have.length", 0);
+    cy.mount(<InputButton noteText="clean floors" />);
+    cy.getByCy("input-button").click();
+    cy.get("@notePostRequest").should(({ request }) => {
+      expect(request.url).to.include("/post");
+      expect(request.method).to.equal("POST");
+      expect(request.body.note).to.contain("clean floors");
+    });
+    cy.get("@notePostRequest.all").should("have.length", 1);
+  });
 });
--- src/component/inputButton/InputButton.jsx
+++ src/component/inputButton/InputButton.jsx
@@ -1,7 +1,25 @@
 import "./InputButton.css";
+import propTypes from "prop-types";
 
-const InputButton = () => {
-  return <button data-cy="input-button">Add Note!</button>;
+const InputButton = ({ noteText }) => {
+  const handleClick = () => {
+    fetch("/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ note: noteText }),
+    });
+    // Reset the state of form back to ""
+  };
+
+  return (
+    <button onClick={handleClick()} data-cy="input-button">
+      Add Note!
+    </button>
+  );
 };
+
+InputButton.propTypes = { noteText: propTypes.string };
 
 export default InputButton;
